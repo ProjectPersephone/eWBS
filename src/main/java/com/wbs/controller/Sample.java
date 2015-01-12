758183e410d1c3ee7e408b401dbd2069e4c0d885
Administrator <Administrator@10.242.118.107>
2015-01-12 17:01:23 +0530

    Sham > Project configuration

--- src/main/java/com/wbs/controller/Sample.java
+++ src/main/java/com/wbs/controller/Sample.java
@@ -1,5 +1,18 @@
 package com.wbs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/sample")
 public class Sample {
 
+	@RequestMapping("/{msg}")
+	@ResponseBody
+	public String check(@PathVariable("msg") String msg) {
+		System.out.println("Hello");
+		return "Welcome : " + msg;
+	}
 }
