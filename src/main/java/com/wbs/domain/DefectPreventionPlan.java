1d925761199d892d12c32adf5cc58ff3e594ac40
Administrator <Administrator@10.242.118.131>
2015-01-14 19:21:27 +0530

    Pooja:DefectPreventionController

---
+++ src/main/java/com/wbs/domain/DefectPreventionPlan.java
@@ -0,0 +1,19 @@
+package com.wbs.domain;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Document(collection = "DefectPreventionPlan")
+public class DefectPreventionPlan extends AuditEntity {
+	
+	private String defectTypeAndDetails;
+	private String preventionPlan;
+	private String currentStatus;
+
+}
