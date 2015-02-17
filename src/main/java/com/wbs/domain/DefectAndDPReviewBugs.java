5620e779b7e37f1b27a1dac52c7b3f666536a554
Pradeep Rawate <Pradeep Rawate@PradeepRawate.nisum.com>
2015-02-17 15:38:18 +0530

    Pradeep> removed unused Imports from files...

--- src/main/java/com/wbs/domain/DefectAndDPReviewBugs.java
+++ src/main/java/com/wbs/domain/DefectAndDPReviewBugs.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
